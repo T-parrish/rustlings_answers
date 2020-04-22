07fd33af7008b01246dc4b474ca3029f749ef44d
Taylor Parrish <twpearish@gmail.com>
2020-04-22 16:30:23 -0700

    all finished rustlings exercises

---
+++ clippy/target/debug/deps/clippy2-0d86c3c9c4ad52db.d
@@ -0,0 +1,5 @@
+/Users/Marklar/Desktop/rustlings/exercises/clippy/target/debug/deps/clippy2-0d86c3c9c4ad52db.rmeta: clippy2.rs
+
+/Users/Marklar/Desktop/rustlings/exercises/clippy/target/debug/deps/clippy2-0d86c3c9c4ad52db.d: clippy2.rs
+
+clippy2.rs:
